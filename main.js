5cf8d9144dc688363afd4f698b6aaa5a07ff41a4
Gabriel-PachecoSS <146880765+Gabriel-PachecoSS@users.noreply.github.com>
2024-02-01 19:31:23 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,10 @@
-/*const formulario = document.getElementById('formNumber')
-let valorA = document.getElementById(campoA)
-let valorB = document.getElementById(campoB)
-
-function validaNumero() {
-    
-
-    valorA = parseFloat(valorA)
-    valorB = parseFloat(valorB)
-    
-    if (valorB > valorA) {
-        alert("Formulário válido", "valid")
-    }
-    else {
-        alert("Formulário inválido, o número do campo B deve ser maior que o inserido no campo A", "invalid")
-    }
-}
-*/
-
 function validarFormulario() {
-    // Obtém os valores dos campos A e B
     var valorA = document.getElementById("campoA").value;
     var valorB = document.getElementById("campoB").value;
 
-    // Converte os valores para números
     valorA = parseFloat(valorA);
     valorB = parseFloat(valorB);
 
-    // Verifica se B é maior que A
     if (valorB > valorA) {
         exibirMensagem("Formulário válido!", "valid");
     } else {
